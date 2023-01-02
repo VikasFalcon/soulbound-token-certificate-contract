e3106ec5e7fae0b1c0fa827f891bb5294689701b
vikasfalcon <vikas.sawant1592@gmail.com>
2023-01-02 12:54:03 +0530

    - Certificate Create method added - Get/Verify certificate method added

--- contracts/SoulboundCertificate.sol
+++ contracts/SoulboundCertificate.sol
@@ -10,17 +10,55 @@ contract SoulboundCertificate is ERC721, Ownable{
 
     Counters.Counter private _tokenIdCounter;
 
+    struct CERTIFICATE{
+        address holder_address;
+        string holder_name;
+        string course_name;
+        uint256 issue_date;
+        uint256 expiry_date;
+    }
+
+    // mapping of tokenId and certificate details
+    mapping(uint256 => CERTIFICATE) private certificateDetails;
+
     constructor() ERC721("Soulbound Certificate Token","SBCT"){
         // TokenId counter start from 1
         _tokenIdCounter.increment();
     }
 
-    function safeMint(address to) public onlyOwner{
+    function createCertificate(address _holderAddress,
+                               string memory _holderName,
+                               string memory _courseName,
+                               uint256 _expiryDate) public onlyOwner{
+        require(_holderAddress != address(0),"Error: Certificate holder address should be valid!");
+
         uint256 tokenId = _tokenIdCounter.current();
+
+        certificateDetails[tokenId].holder_address = _holderAddress;
+        certificateDetails[tokenId].holder_name = _holderName;
+        certificateDetails[tokenId].course_name = _courseName;
+        certificateDetails[tokenId].issue_date = block.timestamp;
+        certificateDetails[tokenId].expiry_date = _expiryDate;
+
+
         _tokenIdCounter.increment();
 
-        _safeMint(to,tokenId,"");
+        _safeMint(_holderAddress,tokenId,"");
     } 
+
+    function getCertificateDetails(uint256 tokenId, address holderAddress) external view returns(address cert_holder_address,
+                                                                          string memory cert_holder_name,
+                                                                          string memory cert_course_name,
+                                                                          uint256 cert_issue_date,
+                                                                          uint256 cert_expiry_date){
+        require(_exists(tokenId) && certificateDetails[tokenId].holder_address == holderAddress,"Error: Certificate does not exists!");
+
+        return (certificateDetails[tokenId].holder_address,
+                certificateDetails[tokenId].holder_name,
+                certificateDetails[tokenId].course_name,
+                certificateDetails[tokenId].issue_date,
+                certificateDetails[tokenId].expiry_date);                                                                     
+    }
 
     function burn(uint256 tokenId) external {
         require(msg.sender == ownerOf(tokenId),"Error: Only token owner can burn token!");
